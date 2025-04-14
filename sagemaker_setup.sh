cac1b88558d7145f3340f599bbba30a5c0ca31af
zj-zhang <zzj.zju@gmail.com>
2025-04-14 14:02:53 +0000

    add sagemaker setup sh script

---
+++ sagemaker_setup.sh
@@ -0,0 +1,23 @@
+# for sagemaker setup, we avoid messing with system-level installations
+apt-get update -y
+#apt-get install tmux -y
+#apt-get install python3-dev -y
+
+apt-get install libaio-dev libopenmpi-dev python3-mpi4py -y
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+source $HOME/.local/bin/env
+
+uv venv p2l_env --python 3.10
+
+source p2l_env/bin/activate
+
+#uv pip install wheel packaging
+
+#uv pip install -r train_requirements.txt
+#uv pip install flash-attn==2.5.9.post1 --no-build-isolation
+
+uv pip install fastapi uvicorn mpi4py
+python -m ipykernel install --user --name=p2l_env --display-name "Python (p2l_env)"
+
